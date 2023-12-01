5f8b6081acee4fde987600a587fcdcc386b53cc4
Le Thi Ngoc Lan <90277811+lnlan1810@users.noreply.github.com>
2023-12-01 22:16:01 +0300

    Task 6

---
+++ 6.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <limits.h>
+#include <unistd.h>
+
+// Функция для вывода списка файлов и подкаталогов в указанном пути
+void listDirectory(const char *path) {
+    DIR *dir;
+    struct dirent *entry;
+
+    // Открываем соединение с каталогом
+    dir = opendir(path);
+
+    // Проверяем ошибки при открытии каталога
+    if (dir == NULL) {
+        perror("Error opening directory");
+        exit(EXIT_FAILURE);
+    }
+
+    // Выводим содержимое каталога
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_name[0] != '.') {
+            printf("%s\n", entry->d_name);
+        }
+    }
+
+    // Закрываем соединение с каталогом
+    if (closedir(dir) == -1) {
+        perror("Error closing directory");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Главная функция программы
+int main(int argc, char *argv[]) {
+    // Проверяем количество аргументов (путь к каталогу)
+    if (argc < 2) {
+        fprintf(stderr,"Usage: %s <path to directory>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Получаем путь к текущему каталогу
+    char currentPath[PATH_MAX];
+    if (getcwd(currentPath, sizeof(currentPath)) == NULL) {
+        perror("Error getting current directory path");
+        exit(EXIT_FAILURE);
+    }
+
+    // Выводим содержимое текущего каталога
+    printf("Contents of the current directory:\n");
+    listDirectory(currentPath);
+
+    // Проверяем и выводим содержимое указанного каталога
+    DIR *dir = opendir(argv[1]);
+    if (dir == NULL) {
+        perror("Error opening specified directory");
+        exit(EXIT_FAILURE);
+    }
+    closedir(dir);
+
+    printf("\nContents of the specified directory (%s):\n", argv[1]);
+    listDirectory(argv[1]);
+
+    printf("\n");
+    return 0;
+}
